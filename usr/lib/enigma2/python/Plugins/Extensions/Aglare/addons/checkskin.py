cca65f51efbd438974c76c4113c4cfbf8bd3fde9
levi-45 <levi45@hotmail.co.uk>
2024-12-23 14:43:14 +0000

    .

--- usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/checkskin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/checkskin.py
@@ -25,18 +25,28 @@ colorend = '\033[m'
 colorstart = '\033[31m'
 PY3 = sys.version_info.major >= 3
 pythonFull = float("%s.%s" % (sys.version_info.major, sys.version_info.minor))
+tmplog = '/tmp/'
 mvi = '/usr/share/'
 cur_skin = config.skin.primary_skin.value.replace('/skin.xml', '')
+skin_base_fold = "%senigma2/%s/" % (mvi, cur_skin)
+user_skin_file = tmplog + 'merged_' + cur_skin + '.xml'
+user_log = tmplog + 'my_debug.log'
+
 
 
 # Funzione di logging
+with open(user_log, "w", encoding="utf-8") as log_file:
+    log_file.write("Log Initialized\n")
+
+
 def checklogskin(data):
     try:
-        print(colorstart + str(data) + colorend)
-        with open("/tmp/my_debug.log", "a", encoding="utf-8") as log_file:
-            log_file.write("\n:> " + str(data))
+        print(colorstart + str(data) + colorend)  # stampa sul terminale
+        with open(user_log, "a", encoding="utf-8") as log_file:
+            log_file.write("\n:> " + str(data))  # scrive nel file di log
     except Exception as e:
-        print("Error logging data: %s" % str(e))
+        print("Error logging data: %s" % str(e))  # gestisce gli errori
+        print("Logging failed!")
 
 
 # Legge un file XML ignorando i commenti
@@ -104,30 +114,50 @@ def checkComponent(myContent, look4Component, myPath, found_files):
         checklogskin("Error in checkComponent: %s" % str(e))
 
 
+"""
+# def find_unused_images(base_path, used_files):
+    # unused_images = []
+
+    # def upShowFile(name):
+        # checklogskin("Unused images found: %s" % name)
+
+    # for root, dirs, files in os.walk(base_path):
+        # for file in files:
+            # if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                # full_path = os.path.join(root, file)
+                # if full_path not in used_files:
+                    # # unused_images.append(full_path)
+                    # upShowFile(full_path)
+    # # return unused_images
+"""
+
+
 # Trova i file immagine non utilizzati
 def find_unused_images(base_path, used_files):
     unused_images = []
 
     def upShowFile(name):
-        checklogskin("Unused images found: %s" % name)
+        unused_images.append(name)  # Aggiungi il file alla lista degli inutilizzati
 
     for root, dirs, files in os.walk(base_path):
         for file in files:
-            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):  # Verifica se il file è un'immagine
                 full_path = os.path.join(root, file)
-                if full_path not in used_files:
-                    # unused_images.append(full_path)
-                    upShowFile(full_path)
-    # return unused_images
+                if full_path not in used_files:  # Se il file non è nella lista dei file usati
+                    upShowFile(full_path)  # Aggiungi il file alla lista di quelli non usati
+
+    if unused_images:
+        checklogskin("Unused images found:")
+        for image in unused_images:
+            checklogskin(image)
 
 
 # Controlla la skin e i file correlati
 def check_module_skin():
     # path = "%senigma2/%s" % (mvi, cur_skin)
-    skin_base_fold = "%senigma2/%s/" % (mvi, cur_skin)
-    user_skin_file = '/tmp/merged_' + cur_skin + '.xml'
-    user_log = '/tmp/my_debug.log'
-
+    # skin_base_fold = "%senigma2/%s/" % (mvi, cur_skin)
+    # user_skin_file = '/tmp/merged_' + cur_skin + '.xml'
+    # user_log = '/tmp/my_debug.log'
     if fileExists(user_skin_file):
         remove(user_skin_file)
     if fileExists(user_log):
@@ -160,10 +190,11 @@ def check_module_skin():
         checkComponent(user_skin, 'image', resolveFilename(SCOPE_GUISKIN, ''), used_files)
 
         # Trova immagini non utilizzate
-        find_unused_images(skin_base_fold, used_files)
-        # unused_images = find_unused_images(skin_base_fold, used_files)
-        # if unused_images:
-            # checklogskin("Unused images found: %s" % unused_images)
+        unused_images = find_unused_images(skin_base_fold, used_files)
+        if unused_images:
+            checklogskin("Unused images found:")
+            for image in unused_images:
+                checklogskin(image)
 
     except Exception as e:
         checklogskin("Error in check_module_skin: %s" % str(e))
