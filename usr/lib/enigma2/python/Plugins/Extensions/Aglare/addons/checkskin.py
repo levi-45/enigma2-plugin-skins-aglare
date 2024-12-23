351977b5124207c8192ceec588f0c0b0a9f18f02
levi-45 <levi45@hotmail.co.uk>
2024-12-23 14:02:55 +0000

    .

--- usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/checkskin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Aglare/addons/checkskin.py
@@ -6,6 +6,7 @@
 #  distributed other than under the conditions noted above.
 #  Lululla coder 2022.07.20
 # NOT REMOVE DISCLAIMER!!!
+# Made from @Lululla 122023 - v.1.2
 
 from __future__ import absolute_import
 from Components.config import config
@@ -19,36 +20,110 @@ import os
 import re
 import sys
 from os import remove
+
 colorend = '\033[m'
 colorstart = '\033[31m'
 PY3 = sys.version_info.major >= 3
-pythonFull = float(str(sys.version_info.major) + "." + str(sys.version_info.minor))
+pythonFull = float("%s.%s" % (sys.version_info.major, sys.version_info.minor))
 mvi = '/usr/share/'
 cur_skin = config.skin.primary_skin.value.replace('/skin.xml', '')
 
 
+# Funzione di logging
 def checklogskin(data):
-    print(colorstart + str(data) + colorend)
-    with open("/tmp/my_debug.log", "a") as log_file:
-        log_file.write("\n:> " + str(data))
+    try:
+        print(colorstart + str(data) + colorend)
+        with open("/tmp/my_debug.log", "a", encoding="utf-8") as log_file:
+            log_file.write("\n:> " + str(data))
+    except Exception as e:
+        print("Error logging data: %s" % str(e))
 
 
-def crea_file_temporaneo(xml_files, temp_path="/tmp/merged.xml"):
-    with open(temp_path, 'w', encoding='utf-8') as temp_file:
-        for file_path in xml_files:
-            with open(file_path, 'r', encoding='utf-8') as xml_file:
-                temp_file.write(xml_file.read())
-    return temp_path
+# Legge un file XML ignorando i commenti
+def readXMLfile(XMLfilename):
+    try:
+        myPath = os.path.realpath(XMLfilename)
+        if not os.path.exists(myPath):
+            return ''
+
+        filecontent = ''
+        inittag = '<!--'
+        endtag = '-->'
+
+        with open(myPath, "r", encoding="utf-8") as myFile:
+            for line in myFile:
+                if inittag in line and endtag in line:
+                    continue
+                filecontent += line
+        return filecontent
+
+    except Exception as e:
+        checklogskin("Error reading XML file %s: %s" % (XMLfilename, str(e)))
+        return ''
 
 
+# Verifica i componenti e i file necessari
+def checkComponent(myContent, look4Component, myPath, found_files):
+    checklogskin("RESEARCH IN PROGRESS...")
+
+    def upShowFile(name):
+        checklogskin("Missing component found: %s" % name)
+
+    try:
+        r = re.findall(r' %s="([a-zA-Z0-9_/\.]+)"' % look4Component, myContent)
+        r = list(set(r))
+        checklogskin("I found components: %s" % r)
+
+        if r:
+            for component in r:
+                full_component_path = os.path.join(myPath, component)
+
+                # Gestione dei componenti Renderer e Converter
+                if look4Component in ['render', 'Convert']:
+                    compiled_extension = ".pyc" if PY3 else ".pyo"
+                    if not fileExists(full_component_path + compiled_extension) and fileExists(full_component_path + ".py"):
+                        upShowFile(full_component_path + compiled_extension)
+
+                # Gestione di immagini e pixmap
+                elif look4Component in ['pixmap', 'image']:
+                    if component.startswith('/'):
+                        # Percorso assoluto
+                        if not os.path.exists(component):
+                            upShowFile(component)
+                        else:
+                            found_files.add(component)
+                    else:
+                        # Percorso relativo
+                        relative_path = os.path.join(mvi, "enigma2", cur_skin, component)
+                        if not os.path.exists(relative_path):
+                            upShowFile(relative_path)
+                        else:
+                            found_files.add(relative_path)
+
+    except Exception as e:
+        checklogskin("Error in checkComponent: %s" % str(e))
+
+
+# Trova i file immagine non utilizzati
+def find_unused_images(base_path, used_files):
+    unused_images = []
+
+    def upShowFile(name):
+        checklogskin("Unused images found: %s" % name)
+
+    for root, dirs, files in os.walk(base_path):
+        for file in files:
+            if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+                full_path = os.path.join(root, file)
+                if full_path not in used_files:
+                    # unused_images.append(full_path)
+                    upShowFile(full_path)
+    # return unused_images
+
+
+# Controlla la skin e i file correlati
 def check_module_skin():
-    path = "%senigma2/%s" % (mvi, cur_skin)
-    listDir = []
-    for (root, dirs, files) in os.walk(path):
-        listDir.extend(dirs)
-
-    user_skin = ""
-    user_skin2 = ""
+    # path = "%senigma2/%s" % (mvi, cur_skin)
     skin_base_fold = "%senigma2/%s/" % (mvi, cur_skin)
     user_skin_file = '/tmp/merged_' + cur_skin + '.xml'
     user_log = '/tmp/my_debug.log'
@@ -62,85 +137,35 @@ def check_module_skin():
     checklogskin("skin_base_fold %s" % skin_base_fold)
     checklogskin("python ver. %s" % pythonFull)
 
-    # Unisci i contenuti XML da skin_base_fold
-    for f in os.listdir(skin_base_fold):
-        if f.endswith('.xml'):
-            user_skin += readXMLfile(os.path.join(skin_base_fold, f))
+    try:
+        user_skin = ""
+        used_files = set()
 
-    # Verifica i file extra nelle sottocartelle di skin_base_fold
-    for root, dirs, files in os.walk(skin_base_fold):
-        for f in files:
-            if f.endswith('.xml'):
-                user_skin2 += readXMLfile(os.path.join(root, f))
+        # Unisci i contenuti XML da skin_base_fold
+        for root, dirs, files in os.walk(skin_base_fold):
+            for f in files:
+                if f.endswith('.xml'):
+                    user_skin += readXMLfile(os.path.join(root, f))
 
-    if user_skin:
-        user_skin = "<skin>\n" + user_skin + '\n' + user_skin2 + "</skin>\n"
-        with open(user_skin_file, "w") as myFile:
-            checklogskin("write myFile %s" % user_skin_file)
-            myFile.write(user_skin)
+        if user_skin:
+            user_skin = "<skin>\n" + user_skin + "</skin>\n"
+            with open(user_skin_file, "w", encoding="utf-8") as myFile:
+                checklogskin("write myFile %s" % user_skin_file)
+                myFile.write(user_skin)
 
-    # Esegui `checkComponent` su ciascun tipo
-    checkComponent(user_skin, 'render', resolveFilename(SCOPE_PLUGINS, '../Components/Renderer/'))
-    checkComponent(user_skin, 'Convert', resolveFilename(SCOPE_PLUGINS, '../Components/Converter/'))
-    checkComponent(user_skin, 'pixmap', resolveFilename(SCOPE_GUISKIN, ''))
-    checkComponent(user_skin, 'image', resolveFilename(SCOPE_GUISKIN, ''))
+        # Esegui `checkComponent` su ciascun tipo
+        checkComponent(user_skin, 'render', resolveFilename(SCOPE_PLUGINS, '../Components/Renderer/'), used_files)
+        checkComponent(user_skin, 'Convert', resolveFilename(SCOPE_PLUGINS, '../Components/Converter/'), used_files)
+        checkComponent(user_skin, 'pixmap', resolveFilename(SCOPE_GUISKIN, ''), used_files)
+        checkComponent(user_skin, 'image', resolveFilename(SCOPE_GUISKIN, ''), used_files)
+
+        # Trova immagini non utilizzate
+        find_unused_images(skin_base_fold, used_files)
+        # unused_images = find_unused_images(skin_base_fold, used_files)
+        # if unused_images:
+            # checklogskin("Unused images found: %s" % unused_images)
+
+    except Exception as e:
+        checklogskin("Error in check_module_skin: %s" % str(e))
 
     checklogskin("==FINISH CHECK MY SKIN %s==" % cur_skin)
-
-
-def checkComponent(myContent, look4Component, myPath):
-    checklogskin("RESEARCH IN PROGRESS...")
-
-    def upShowFile(name):
-        checklogskin("Missing component found: %s" % name)
-
-    r = re.findall(r' %s="([a-zA-Z0-9_/\.]+)"' % look4Component, myContent)
-    r = list(set(r))
-    checklogskin("I found components: %s" % r)
-
-    if r:
-        try:
-            checklogskin("Research for %s components" % look4Component)
-            for component in r:
-                component_path = component
-                full_component_path = myPath + component_path
-
-                # Gestione dei componenti Renderer e Converter
-                if look4Component in ['render', 'Convert']:
-                    compiled_extension = ".pyc" if PY3 else ".pyo"
-                    if not fileExists(full_component_path + compiled_extension) and fileExists(full_component_path + ".py"):
-                        upShowFile(full_component_path + compiled_extension)
-
-                # Gestione di immagini e pixmap
-                elif look4Component in ['pixmap', 'image']:
-                    if component.startswith('/'):
-                        # Percorso assoluto
-                        if not os.path.exists(component_path):
-                            upShowFile(component_path)
-                    else:
-                        # Percorso relativo
-                        relative_path = os.path.join(mvi, "enigma2", cur_skin, component_path)
-                        if not os.path.exists(relative_path):
-                            upShowFile(relative_path)
-        except Exception as e:
-            error_message = "Error:", str(e)
-            print(error_message)
-            checklogskin(error_message)
-    return
-
-
-def readXMLfile(XMLfilename):
-    myPath = os.path.realpath(XMLfilename)
-    if not os.path.exists(myPath):
-        return ''
-
-    filecontent = ''
-    inittag = '<!--'
-    endtag = '-->'
-
-    with open(myPath, "r") as myFile:
-        for line in myFile:
-            if inittag in line and endtag in line:
-                continue
-            filecontent += line
-    return filecontent
