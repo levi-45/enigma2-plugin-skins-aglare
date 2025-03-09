a35ba8d4d3b58283c92f01fd4872f8910e967b20
levi-45 <levi45@hotmail.co.uk>
2025-03-09 08:59:18 +0000

    .

--- usr/lib/enigma2/python/Plugins/Extensions/Aglare/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/Aglare/plugin.py
@@ -45,7 +45,7 @@ else:
     from urllib2 import Request
 
 
-version = '5.2'
+version = '5.3'
 my_cur_skin = False
 cur_skin = config.skin.primary_skin.value.replace('/skin.xml', '')
 OAWeather = resolveFilename(SCOPE_PLUGINS, "Extensions/{}".format('OAWeather'))
